c46d5ce3f9199e7a155bada3590448c760faf694
julia <jufedorishcheva@gmail.com>
2022-02-10 17:23:51 +0400

    задача "Готовимся к миссии"

--- 1.py
+++ 1.py
@@ -3,14 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
     param = {}
     param['title'] = 'Домашняя страница'
-    return render_template('index.html', **param)
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
-
