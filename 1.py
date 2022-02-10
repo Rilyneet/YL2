6fccdf5ed291d728002e4b0df806909bfdf410e3
julia <jufedorishcheva@gmail.com>
2022-02-10 17:10:46 +0400

    Initial commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    param = {}
+    param['title'] = 'Домашняя страница'
+    return render_template('index.html', **param)
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1')
